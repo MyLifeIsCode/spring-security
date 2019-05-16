808dbe15b73b0615c61a8f93afe7243ec851443e
qll <249180439@qq.com>
2019-05-16 14:16:29 +0800

    edit

--- src/main/java/com/myself/security/controller/login/LoginController.java
+++ src/main/java/com/myself/security/controller/login/LoginController.java
@@ -1,7 +1,12 @@
 package com.myself.security.controller.login;
 
+import com.myself.security.domain.user.UserInfo;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import sun.plugin.liveconnect.SecurityContextHelper;
 
 @Controller
 public class LoginController {
@@ -9,5 +14,17 @@ public class LoginController {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/getUserName")
+    public String getUserName(Model model){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal.equals("anonymousUser")){
+            model.addAttribute("name","anonymous");
+        }else {
+            User user = (User)principal;
+            model.addAttribute("name",user.getUsername());
+        }
+        return "/index";
     }
 }
